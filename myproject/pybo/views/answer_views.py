0fa966b2d1e8022e1dfdb6313f0302e77cefdda1
DESKTOP-VT5N6G2\USER <kay061476@gmail.com>
2022-09-20 02:37:36 +0900

    0919_2

--- myproject/pybo/views/answer_views.py
+++ myproject/pybo/views/answer_views.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
-from flask import Blueprint, url_for, request
+from flask import Blueprint, url_for, request, render_template
 from werkzeug.utils import redirect
 
+
 from pybo import db
+from ..forms import AnswerForm
 from pybo.models import Question, Answer
 
 bp = Blueprint('answer', __name__, url_prefix='/answer')
@@ -11,11 +13,12 @@ bp = Blueprint('answer', __name__, url_prefix='/answer')
 
 @bp.route('/create/<int:question_id>', methods=('POST','GET'))
 def create(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    content = request.form['content']
-    answer = Answer(question=question, content=content, create_date=datetime.now())
-    db.session.add(answer)
-    # answer = Answer(content=content, create_date=datetime.now())
-    # question.answer_set.append(answer)
-    # db.session.commit()
-    return redirect(url_for('question.detail', question_id=question_id))
+    if form.validate_on_submit():
+        content = request.form['content']
+        answer = Answer(content=content, create_date=datetime.now())
+        question.answer_set.append(answer)
+        db.session.commit()
+        return redirect(url_for('question.detail', question_id=question_id))
+    return render_template('question/question_detail.html', question=question, form=form)
--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from .. import db
 from pybo.models import Question
-from pybo.forms import QuestionForm
+from pybo.forms import QuestionForm, AnswerForm
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -17,10 +17,11 @@ def _list():
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
+    form = AnswerForm()
     question = Question.query.get_or_404(question_id)
-    return render_template('question/question_detail.html', question=question)
+    return render_template('question/question_detail.html', question=question, form=form)
 
-@bp.route('/create/')
+@bp.route('/create/', methods=('GET', 'POST'))
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
