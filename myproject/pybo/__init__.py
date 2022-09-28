b5109e5e1b7f4664920c0281dae899e14d0d2e49
DESKTOP-0M1SFST\user <kay061476@gmail.com>
2022-09-28 18:02:43 +0900

    0928

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
 import config
 
-db = SQLAlchemy()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
 
 def create_app():
     app = Flask(__name__)
@@ -13,7 +22,10 @@ def create_app():
 
     #ORM
     db.init_app(app)
-    migrate.init_app(app, db)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
     
     from . import models
 
